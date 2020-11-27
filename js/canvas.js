0e991dffd5982dccbc70186900c2b7815e982c9e
Per Møller <public.pfm@gmail.com>
2020-11-27 21:45:15 +0100

    TypeScript

---
+++ js/canvas.js
@@ -0,0 +1,249 @@
+export function initCanvas(canvasElement) {
+    const rectContainsPoint = (rect, point) => point.x >= rect.x && point.x <= (rect.x + rect.w) && point.y >= rect.y && point.y <= (rect.y + rect.h);
+    /**
+     * @returns A rect surrounding all the rects.
+     */
+    const rectMerge = (rects) => {
+        const minX = Math.min(...rects.map(r => r.x));
+        const minY = Math.min(...rects.map(r => r.y));
+        const maxX = Math.max(...rects.map(r => r.x));
+        const maxY = Math.max(...rects.map(r => r.y));
+        return {
+            x: minX,
+            y: minY,
+            w: maxX - minX,
+            h: maxY - minY
+        };
+    };
+    class Control {
+        constructor(ctx) {
+            this.ctx = ctx;
+            this.handleEvent = _ => this;
+            this.draw = () => { };
+            this.measureRect = () => ({ x: 0, y: 0, h: 0, w: 0 });
+        }
+    }
+    class Rect extends Control {
+        constructor(ctx, rect) {
+            super(ctx);
+            this.draw = () => {
+                this.ctx.fillStyle = "#eeeeee";
+                this.ctx.fillRect(this.x, this.y, this.w, this.h);
+                this.ctx.strokeStyle = "#000000";
+                this.ctx.strokeRect(this.x, this.y, this.w, this.h);
+            };
+            this.measureRect = () => this;
+            this.x = rect.x;
+            this.y = rect.y;
+            this.w = rect.w;
+            this.h = rect.h;
+        }
+    }
+    class Text extends Control {
+        constructor(ctx, x, xAlign, y, yAlign, text) {
+            super(ctx);
+            this.x = x;
+            this.xAlign = xAlign;
+            this.y = y;
+            this.yAlign = yAlign;
+            this.text = text;
+            this.draw = () => {
+                this.ctx.textBaseline = this.yAlign;
+                this.ctx.textAlign = this.xAlign;
+                this.ctx.fillStyle = "#000000";
+                this.ctx.fillText(this.text, this.x, this.y);
+            };
+            this.measureRect = () => {
+                const size = this.ctx.measureText(this.text);
+                return { x: this.x, y: this.y, w: size.width, h: parseInt(this.ctx.font) };
+            };
+        }
+    }
+    class Box extends Control {
+        constructor(ctx, rect, mousedown) {
+            super(ctx);
+            this.mousedown = mousedown;
+            this.handleEvent = e => {
+                if (e.type === "mousedown" && !e.handled && e.which === 1 && rectContainsPoint(this.rect, e.point)) {
+                    e.handled = true;
+                    return new Box(this.ctx, this.rect, {
+                        rectPoint: { x: this.rect.x, y: this.rect.y },
+                        mousePoint: e.point
+                    });
+                }
+                else if (e.type === "mousedowncancel" && e.which === 1 && this.mousedown) {
+                    return new Box(this.ctx, {
+                        x: this.mousedown.rectPoint.x,
+                        y: this.mousedown.rectPoint.y,
+                        w: this.rect.w,
+                        h: this.rect.h
+                    }, this.mousedown);
+                }
+                else if (e.type === "mousemove" && this.mousedown) {
+                    return new Box(this.ctx, {
+                        x: this.mousedown.rectPoint.x + (e.point.x - this.mousedown.mousePoint.x),
+                        y: this.mousedown.rectPoint.y + (e.point.y - this.mousedown.mousePoint.y),
+                        w: this.rect.w,
+                        h: this.rect.h
+                    }, this.mousedown);
+                }
+                else if (e.type === "mouseup" && e.which === 1 && this.mousedown) {
+                    return new Box(this.ctx, {
+                        x: this.mousedown.rectPoint.x + (e.point.x - this.mousedown.mousePoint.x),
+                        y: this.mousedown.rectPoint.y + (e.point.y - this.mousedown.mousePoint.y),
+                        w: this.rect.w,
+                        h: this.rect.h
+                    }, undefined);
+                }
+                return this;
+            };
+            this.draw = () => {
+                this.rect.draw();
+            };
+            this.measureRect = () => this.rect;
+            this.rect = new Rect(ctx, rect);
+        }
+    }
+    class ContextmenuItem extends Control {
+        constructor(ctx, rect, text, onClick) {
+            super(ctx);
+            this.onClick = onClick;
+            this.handleEvent = e => {
+                if (e.type === "mouseup" && e.which === 1 && rectContainsPoint(this.rect, e.point)) {
+                    this.onClick();
+                }
+                return this;
+            };
+            this.draw = () => {
+                this.rect.draw();
+                this.text.draw();
+            };
+            this.measureRect = () => this.rect;
+            this.text = new Text(ctx, rect.x + 10, "start", rect.y + rect.h / 2, "middle", text);
+            this.rect = new Rect(ctx, rect);
+        }
+    }
+    class Contextmenu extends Control {
+        constructor(ctx, x, y, onDismiss, items) {
+            super(ctx);
+            this.x = x;
+            this.y = y;
+            this.onDismiss = onDismiss;
+            this.handleEvent = e => {
+                // TODO: Fix newItems
+                //const newItems: ContextmenuItem[] = []
+                const itemsChanged = this.items.reduceRight((changed, item, index) => (( /*newItems[index] =*/item.handleEvent(e)) != item) || changed, false);
+                if (e.type === "mouseup" || e.type === "mousedowncancel") {
+                    this.onDismiss();
+                }
+                if (itemsChanged) {
+                    throw new Error("TODO: itemsChanged in ContextMenu");
+                    //return new Contextmenu(this.ctx, this.x, this.y, this.onDismiss, newItems)
+                }
+                return this;
+            };
+            this.draw = () => {
+                this.items.forEach(i => i.draw());
+            };
+            this.measureRect = () => rectMerge(this.items.map(item => item.measureRect()));
+            const itemWidth = 150;
+            const itemHeight = 20;
+            // adjust context menu to stay within the canvas
+            if (x + itemWidth > ctx.canvas.width) {
+                this.x = ctx.canvas.width - itemWidth;
+            }
+            if (y + items.length * itemHeight > ctx.canvas.height) {
+                this.y = ctx.canvas.height - items.length * itemHeight;
+            }
+            this.items = items.map((item, index) => new ContextmenuItem(ctx, { x: x, y: y + index * itemHeight, w: itemWidth, h: itemHeight }, item.text, item.onClick));
+        }
+    }
+    class Canvas2d {
+        constructor(canvasElement) {
+            this.canvasElement = canvasElement;
+            this.handleEvent = (e) => {
+                if (e.target === this.canvasElement) {
+                    let ignoreEvent = false;
+                    // TODO: Handle keybardEvent
+                    if (e instanceof MouseEvent) {
+                        // if the user press a mouse-button outside the canvas but releases it inside the canvas we ignore the mouseup-event (but not the contextmenu-event)
+                        if (e.type === "mouseup" && !this.mousedown[e.which]) {
+                            ignoreEvent = true;
+                        }
+                        // keep track of the mouse-buttons that are pressed and released inside the canvas
+                        if (e.type === "mousedown") {
+                            this.mousedown[e.which] = true;
+                        }
+                        else if (e.type === "mouseup") {
+                            this.mousedown[e.which] = false;
+                        }
+                        if (e.type === "contextmenu") {
+                            e.preventDefault();
+                            const contextMenu = new Contextmenu(this.ctx, e.offsetX, e.offsetY, () => {
+                                this.controlsToRemove.push(contextMenu);
+                            }, [
+                                {
+                                    text: "box",
+                                    onClick: () => {
+                                        this.controlsToAdd.push(new Box(this.ctx, { x: e.offsetX, y: e.offsetY, w: 150, h: 150 }));
+                                    }
+                                },
+                                { text: "test 2", onClick: () => alert("test 2") },
+                                { text: "test 3", onClick: () => alert("test 3") }
+                            ]);
+                            return [...this.controls, contextMenu];
+                        }
+                        if (!ignoreEvent) {
+                            this.controlsToAdd = [];
+                            this.controlsToRemove = [];
+                            // TODO: Fix cast to any
+                            let controls = this.dispatchEventToControls({ type: e.type, point: { x: e.offsetX, y: e.offsetY }, which: e.which, handled: false });
+                            if (this.controlsToAdd.length > 0) {
+                                controls = [...controls, ...this.controlsToAdd];
+                            }
+                            if (this.controlsToRemove.length > 0) {
+                                controls = controls.filter(c => this.controlsToRemove.indexOf(c) < 0);
+                            }
+                            return controls;
+                        }
+                    }
+                }
+                else {
+                    // if the user press a mouse-button inside the canvas but releases it outside the canvas we tell the components to cancel what they are doing due to the mousedown event
+                    if (e.type === "mouseup" && this.mousedown[e.which]) {
+                        this.mousedown[e.which] = false;
+                        return this.dispatchEventToControls({ type: "mousedowncancel", which: e.which });
+                    }
+                }
+                return this.controls;
+            };
+            this.dispatchEventToControls = (event) => {
+                const newControls = [];
+                const controlsChanged = this.controls.reduceRight((changed, control, index) => ((newControls[index] = control.handleEvent(event)) != control) || changed, false);
+                return controlsChanged ? newControls : this.controls;
+            };
+            // TODO: Fix use of !
+            this.ctx = canvasElement.getContext("2d");
+            // Object with flags indicating which mousebuttons are pressed.
+            this.mousedown = [];
+            this.controls = [];
+            this.controlsToAdd = [];
+            this.controlsToRemove = [];
+            const handleEvent = (e) => {
+                const oldControls = this.controls;
+                const newControls = this.handleEvent(e);
+                if (oldControls != newControls) {
+                    this.controls = newControls;
+                    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+                    this.controls.forEach(c => c.draw());
+                }
+            };
+            document.addEventListener("mousedown", handleEvent);
+            document.addEventListener("mousemove", handleEvent);
+            document.addEventListener("mouseup", handleEvent);
+            document.addEventListener("keydown", handleEvent);
+            document.addEventListener("contextmenu", handleEvent);
+        }
+    }
+    new Canvas2d(canvasElement);
+}
